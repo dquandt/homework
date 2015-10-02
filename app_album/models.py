8294d71338876062767cdc8a7f381768024f8779
dquandt <djquandt@gmail.com>
2015-10-01 20:24:25 -0400

    adding albums to index template

--- app_album/models.py
+++ app_album/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 from PIL import Image 
 import os 
-from settings import MEDIA_ROOT 
-
 
 class Album(models.Model):
 	title = models.CharField(max_length=60)
@@ -21,29 +19,6 @@ class Image(models.Model):
 	albums = models.ManyToManyField(Album, blank=True)
 	user = models.ForeignKey(User, null=True, blank=True)
 
-	# I'm not really sure what this is doing
-	# Resizing the image, somehow 
-	# http://lightbird.net/dbe/photo.html
-	def save(self, *args, **kwargs):
-		super(Image, self).save(*args, **kwargs)
-		im = Image.open(os.path.join(MEDIA_ROOT, self.image.name))
-		self.width, self.height = im.size
-		super(Image, self).save(*args, **kwargs)
-
-	def size(self):
-		return "%s x %x" % (self.width, self.height)
-
-	def __str__(self):
-		return self.image.name
-
-	def albums_(self):
-		lst = [x[1] for x in self.albums.values_list()]
-		return str(join(lst, ', '))
-
-	#this doesn't make any sense either 
-	def thumbnail(self):
-          return """<a href="/media/%s"><img border="0" alt="" src="/media/%s" height="40" /></a>""" % ((self.image.name, self.image.name))
-                                                              
 
 class AlbumAdmin(admin.ModelAdmin):
 	search_fields = ["title"]
@@ -51,12 +26,8 @@ class AlbumAdmin(admin.ModelAdmin):
 
 class ImageAdmin(admin.ModelAdmin):
 	search_fields = ["title"]
-	list_display = ["__str__", "title", "user", "size", "albums_", "thumbnail"]
+	list_display = ["__str__", "title", "user"]
 	list_filter = ["albums", "user"]
-
-	def save_model(self, request, obj, form, change):
-		obj.user = request.user
-		obj.save()
 
 
 
--- app_album/views.py
+++ app_album/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
+from .models import Album 
 
 def index(request):
-		return render(request, 'app_album/index.html', {})
+		albums = Album.objects.all()
+		return render(request, 'app_album/index.html', {'albums' : albums})
