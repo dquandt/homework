ad98bab22a325cb17bccd1f64da89d7fd3e417cd
dquandt <djquandt@gmail.com>
2015-10-29 14:53:40 -0400

    this is to change some test files

--- proj_homework/functional_tests/functional_tests.py
+++ proj_homework/functional_tests/functional_tests.py
@@ -4,7 +4,6 @@ browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
 page_body = browser.find_element_by_tag_name('body')
-
 assert 'this is my album, yo' in page_body.text 
 
 browser.quit()
